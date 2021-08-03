511b335f25e80eea95c5a66ab313c2f55fb4c3d9
fernando-adrian <advancexz@gmail.com>
2021-08-03 00:57:13 -0700

    files uploaded

--- public/js/ticket-form.js
+++ public/js/ticket-form.js
@@ -8,6 +8,11 @@ input_cantidad.addEventListener('input', fn_total);
 input_precio.addEventListener('input', fn_total);
 select_estaciones.addEventListener('input', put_rfc);
 
+// console.log('hola');
+// var floating = 30;
+// var float_fixed = floating.toFixed(2);
+// console.log(float_fixed);
+
 const estaciones = [
     "EST 1790 - BELLAS ARTES GASOLINERA SA DE CV",
     "EST 3719 - CORPORATIVO ENERVISION SAPI DE CV",
@@ -18,7 +23,7 @@ const rfc = [
 ];
 const direccion_1 = [
     "SONORA Y VILLAHERMOSA",
-    "CARR. A LA COLORADA KM. 3.5 ESQ. PLANETARIO",
+    "CARR.A LA COLORADA KM. 3.5 ESQ. PLANETARIO",
 ]
 const direccion_2 = [
     "COL. ESPERANZA, MEXICALI, B.C. CP 21350",
@@ -84,11 +89,11 @@ function imprimir(e){
     var factor_aleatorio = (Math.random() * 100);
     var ventas_calculadas = og_num_venta + atendidos_hipoteticos + factor_aleatorio;
 
-    document.getElementById('ticket_fecha_hora').innerHTML = "FECHA: " + fecha_correcta.getDate() + "/" + (parseInt(fecha_correcta.getMonth()) +1) + "/" + fecha_correcta.getFullYear().toString().substring(-2) + " " + document.getElementById('hora').value;
-    document.getElementById('ticket_cantidad').innerHTML = document.getElementById('cantidad').value;
-    document.getElementById('ticket_precio').innerHTML = document.getElementById('precio').value;
-    document.getElementById('ticket_importe').innerHTML = document.getElementById('total').value;
-    document.getElementById('ticket_total').innerHTML = parseFloat( document.getElementById('total').value).toLocaleString('en-US') + " MXN";
+    document.getElementById('ticket_fecha_hora').innerHTML = "FECHA: " + fecha_correcta.getDate() + "/" + (parseInt(fecha_correcta.getMonth()) +1) + "/" + fecha_correcta.getFullYear().toString().substr(-2) + " " + document.getElementById('hora').value;
+    document.getElementById('ticket_cantidad').innerHTML = parseFloat(document.getElementById('cantidad').value).toFixed(3);
+    document.getElementById('ticket_precio').innerHTML = parseFloat(document.getElementById('precio').value).toFixed(2);
+    document.getElementById('ticket_importe').innerHTML = parseFloat(document.getElementById('total').value).toFixed(2);
+    document.getElementById('ticket_total').innerHTML = parseFloat( document.getElementById('total').value).toLocaleString('es-MX', {minimumFractionDigits:2, maximumFractionDigits:2}) + ' MXN';
     document.getElementById('ticket_precio_con_letra').innerHTML = "SON: "+ NumeroALetras(document.getElementById('total').value) + " M.N.";
     document.getElementById('ticket_estacion').innerHTML = estaciones[document.getElementById('estaciones').value];
     document.getElementById('ticket_rfc').innerHTML = "RFC " + rfc[document.getElementById('estaciones').value];
@@ -96,5 +101,47 @@ function imprimir(e){
     document.getElementById('ticket_direccion_2').innerHTML = direccion_2[document.getElementById('estaciones').value];
     document.getElementById('ticket_num_venta').innerHTML = "NUM VENTA: " + parseInt(ventas_calculadas);
 
+    var qr = new QRCode(document.getElementById("qrcode"),  {
+        text: "T|03719|861464|3|73.374|1686.13|0",
+        width: 64,
+        height: 64,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.L
+    });
     window.print();
+    // qr.clear();
+
+    // var qrious_code = new QRious();
+    // qrious_code = new QRious({
+    //     element: document.getElementById('qrcode'),
+    //     size: 80,
+    //     padding: 0,
+    //     value: 'T|03719|861464|3|73.374|1686.13|0',
+    //     background: '#ffffff',
+    //     foreground: '#000000'
+    //   });
+    // console.log('timeout init');
+    // setTimeout(() => {
+    
+    //     window.print();  
+    //   console.log('timeout end');
+    // }, 1000);
   }
+/*
+  formato del QR
+
+  T|03719|861464|3|73.374|1686.13|0
+
+  T = ?
+  03719 = 0 + numero de estacion
+  861464 = numero de venta
+  3 = DIESEL
+  73.374 = CANT LITROS
+  1686.13 = IMPORTE
+  0 = ?
+
+
+  T|01790|3799314|1|2.360|50.00|0
+  fin
+  */
