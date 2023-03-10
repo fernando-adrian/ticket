cf731a9ff252459b726fe4191c078689324b05bf
fernando robles <advancexz@gmail.com>
2023-03-10 15:06:43 -0800

    oxxo-ticket-form.js updated to remove unused code

--- public/js/oxxo-ticket-form.js
+++ public/js/oxxo-ticket-form.js
@@ -20,20 +20,20 @@ function imprimir(e){
 
     e.preventDefault();
 
-    if (!validaciones())
-        return;
+    // if (!validaciones())
+    //     return;
 
-    var fecha = new Date(document.getElementById('fecha').value);
-    var fecha_offset = fecha.getTimezoneOffset() * 60000;
-    var fecha_correcta = new Date(fecha.getTime() + fecha_offset);
+    // var fecha = new Date(document.getElementById('fecha').value);
+    // var fecha_offset = fecha.getTimezoneOffset() * 60000;
+    // var fecha_correcta = new Date(fecha.getTime() + fecha_offset);
 
-    var date_fixed_2 = fecha_correcta.getDate().toString().padStart(2,'0');
-    var month_fixed_2 = (parseInt(fecha_correcta.getMonth()) +1).toString().padStart(2,'0');
-    var year_fixed_4 = fecha_correcta.getFullYear().toString().substr(-4);
-    var hora_fixed_2_2 = document.getElementById('hora').value;
+    // var date_fixed_2 = fecha_correcta.getDate().toString().padStart(2,'0');
+    // var month_fixed_2 = (parseInt(fecha_correcta.getMonth()) +1).toString().padStart(2,'0');
+    // var year_fixed_4 = fecha_correcta.getFullYear().toString().substr(-4);
+    // var hora_fixed_2_2 = document.getElementById('hora').value;
 
-    document.getElementById('ticket-fecha-hora').innerHTML = date_fixed_2 + "/" + month_fixed_2 + "/" + year_fixed_4 + "&nbsp;&nbsp;&nbsp;" + hora_fixed_2_2;
-    document.getElementById('ticket-importe').innerHTML = "$ " + document.getElementById('importe').value + ".00";
+    // document.getElementById('ticket-fecha-hora').innerHTML = date_fixed_2 + "/" + month_fixed_2 + "/" + year_fixed_4 + "&nbsp;&nbsp;&nbsp;" + hora_fixed_2_2;
+    // document.getElementById('ticket-importe').innerHTML = "$ " + document.getElementById('importe').value + ".00";
     
     // Qrious
     var qrious_code = new QRious();
@@ -49,5 +49,5 @@ function imprimir(e){
 
     setTimeout(() => {
         window.print();  
-    }, 1);
+    }, 100);
   }
