6a1126909f811c2df16bf3e59fc27f200e195b10
fernando robles <advancexz@gmail.com>
2023-03-09 18:28:33 -0800

    firebase related code removed

--- public/js/caseta-ticket-form.js
+++ public/js/caseta-ticket-form.js
@@ -1,47 +1,4 @@
 
-initFirebase();
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      console.log('logged in');
-      document.getElementById('form-box').style.visibility = 'visible';
-    } else {
-      // No user is signed in.
-      console.log('not logged in');
-      document.getElementById('form-box').style.visibility = 'hidden';
-      window.location.href = 'index.html';
-    }
-  });
-
-  document.getElementById('logout').addEventListener('click',cerrar_sesion);
-//   document.getElementById('back').addEventListener('click',back);
-
-function cerrar_sesion(){
-    console.log('logout gallery');
-    firebase.auth().signOut();
-}
-
-// function back(){
-//     console.log('atras');
-//     window.location.href = '/public/ticket-gallery.html';
-// }
-
-function initFirebase(){
-    //import firebase from "firebase/app"
-    var firebaseConfig = {
-        apiKey: "AIzaSyBA_ebBq9qUw8XBBZ5eNh83vRjhhI8HMdw",
-        authDomain: "ticket-go.firebaseapp.com",
-        projectId: "ticket-go",
-        storageBucket: "ticket-go.appspot.com",
-        messagingSenderId: "611776354257",
-        appId: "1:611776354257:web:60ba0681cacbf30cc3f2f0",
-        measurementId: "G-SLCZW5RCN6"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    console.log('Firebase inicializado');
-}
-
 var input_total = document.getElementById('total');
 var select_casetas = document.getElementById('casetas');
 
--- public/js/plaza-de-cobro-form.js
+++ public/js/plaza-de-cobro-form.js
@@ -1,43 +1,6 @@
 
-initFirebase();
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      console.log('logged in');
-      document.getElementById('form-box').style.visibility = 'visible';
-    } else {
-      // No user is signed in.
-      console.log('not logged in');
-      document.getElementById('form-box').style.visibility = 'hidden';
-      window.location.href = 'index.html';
-    }
-  });
-
-  document.getElementById('logout').addEventListener('click',cerrar_sesion);
-
-function cerrar_sesion(){
-    console.log('logout gallery');
-    firebase.auth().signOut();
-}
-
-function initFirebase(){
-    //import firebase from "firebase/app"
-    var firebaseConfig = {
-        apiKey: "AIzaSyBA_ebBq9qUw8XBBZ5eNh83vRjhhI8HMdw",
-        authDomain: "ticket-go.firebaseapp.com",
-        projectId: "ticket-go",
-        storageBucket: "ticket-go.appspot.com",
-        messagingSenderId: "611776354257",
-        appId: "1:611776354257:web:60ba0681cacbf30cc3f2f0",
-        measurementId: "G-SLCZW5RCN6"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    console.log('Firebase inicializado');
-}
-
 function validaciones(){
-
+        
     var message_alert = '';
     if (document.getElementById('fecha').value == "")
         message_alert = 'Por favor selecciona una fecha';
